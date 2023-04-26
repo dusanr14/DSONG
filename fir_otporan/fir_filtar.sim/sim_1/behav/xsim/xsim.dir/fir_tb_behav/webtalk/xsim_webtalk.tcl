6311ac2148287f07b70b0d349d8d158023e932f0
dusanr14 <dusanr14@gmail.com>
2023-04-26 22:11:36 +0200

    Ispravljena verzija projekta

--- fir_otporan/fir_filtar.sim/sim_1/behav/xsim/xsim.dir/fir_tb_behav/webtalk/xsim_webtalk.tcl
+++ fir_otporan/fir_filtar.sim/sim_1/behav/xsim/xsim.dir/fir_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/Dusan/Desktop/DSONG/fir_otporan/fir_filtar.sim/sim_1/behav/xsim/xsim.dir/fir_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Wed Apr 12 18:24:17 2023" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Wed Apr 26 21:38:19 2023" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "b494059c-949d-4a09-b814-9a9101debbce" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "bb4f5cad11e34e0e93356839d4993a5d" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "108" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "114" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD Ryzen 5 3500U with Radeon Vega Mobile Gfx  " -context "user_environment"
@@ -36,7 +36,7 @@ webtalk_add_data -client xsim -key Total_Nets -value "0" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Total_Processes -value "914" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Total_Instances -value "133" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Xilinx_HDL_Libraries_Used -value "secureip " -context "xsim\\usage"
-webtalk_add_data -client xsim -key Compiler_Time -value "2.36_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Compiler_Memory -value "78472_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3954295920 -regid "" -xml C:/Users/Dusan/Desktop/DSONG/fir_otporan/fir_filtar.sim/sim_1/behav/xsim/xsim.dir/fir_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Dusan/Desktop/DSONG/fir_otporan/fir_filtar.sim/sim_1/behav/xsim/xsim.dir/fir_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Dusan/Desktop/DSONG/fir_otporan/fir_filtar.sim/sim_1/behav/xsim/xsim.dir/fir_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Compiler_Time -value "1.89_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Compiler_Memory -value "78524_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 2687186673 -regid "" -xml C:/Users/Dusan/Desktop/DSONG/fir_otporan/fir_filtar.sim/sim_1/behav/xsim/xsim.dir/fir_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Dusan/Desktop/DSONG/fir_otporan/fir_filtar.sim/sim_1/behav/xsim/xsim.dir/fir_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Dusan/Desktop/DSONG/fir_otporan/fir_filtar.sim/sim_1/behav/xsim/xsim.dir/fir_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
--- fir_otporan/force_signals.tcl
+++ fir_otporan/force_signals.tcl
@@ -1,7 +1,11 @@
 remove_forces {/fir_tb/uut_fir_filter/\first_section_redundancy(1)\/first_section/sec_o}
+remove_forces {/fir_tb/uut_fir_filter/\first_section_redundancy(0)\/first_section/b_i}
 remove_forces {/fir_tb/uut_fir_filter/\first_section_redundancy(1)\/first_section/b_i}
+remove_forces {/fir_tb/uut_fir_filter/\first_section_redundancy(2)\/first_section/b_i}
+remove_forces {/fir_tb/uut_fir_filter/\first_section_redundancy(3)\/first_section/b_i}
+add_force {/fir_tb/uut_fir_filter/\first_section_redundancy(0)\/first_section/sec_o} -radix hex 11111 200us
 add_force {/fir_tb/uut_fir_filter/\first_section_redundancy(1)\/first_section/sec_o} -radix hex 11111 200us
-
-run 350us
+add_force {/fir_tb/uut_fir_filter/\first_section_redundancy(2)\/first_section/sec_o} -radix hex 11111 200us
+run 300us
 
 
